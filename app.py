c5d42550c6098cae8392da55b04cde7347198205
41023236 <nfu41023236@gmail.com>
2025-06-02 14:56:48 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(page_title="國立虎尾科技大學機械設計工程系", layout="wide")
+
+st.title("113(下)『電腦視覺(Computer Vision)』課程期末報告")
+
+st.title("電腦視覺之物件辨識實作")
+
+st.header("指導老師：周榮源")
+st.header("班級：四設三乙")
+st.header("組別：第14組")
+st.header("組員：41023236/黃柏閎、41023235/黃俊錡、41136131/羅嘉元")
+
+
+st.write("歡迎來到熱流力實驗報告。請從左側選單選擇要查看的實驗項目。")
+
+# 不需要在這裡定義實驗項目列表，因為Streamlit會自動生成側邊欄
+
+# 顯示一張圖片(image)
+st.image("files/NFU.png")
+# Caption
+st.caption("""(這是國立虎尾科技大學之校門!)記得在影片或是照片後加上圖例說明!""")
+
+
+
